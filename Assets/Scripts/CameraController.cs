01e09beca00e4bc93819ff65bdb6561916b44c15
agyscon 67 <agysconner67@gmail.com>
2019-01-01 21:07:19 -0500

    Added Camera and Player Controller Scripts

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField] private Transform playerTransform;
+    private PlayerMovementControls playerMovementControls;
+
+    // Vector from player to camera
+    private Vector3 cameraVector;
+
+    void Awake() {
+        cameraVector = new Vector3(3f, 2f, -4f);
+        playerMovementControls = playerTransform.gameObject.GetComponent<PlayerMovementControls>();
+    }
+
+    void Update() {
+        Vector3 lookInput = new Vector3(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"), 0);
+        if (lookInput.magnitude > 1) {
+            lookInput.Normalize();
+        }
+        lookInput *= 10;
+        RotateCamera(lookInput);
+        if (playerMovementControls != null) {
+            playerMovementControls.PlayerMove(cameraVector);
+        } else {
+            Debug.LogError("No player movement control script found.");
+        }
+    }
+
+    private void RotateCamera(Vector3 angle) {
+        // Ortho projection of camera vector onto xz plane
+        Vector3 xzRing = new Vector3(cameraVector.x, 0, cameraVector.z);
+        float xAngle = Vector3.Angle(Vector3.forward, xzRing);
+        if (cameraVector.x < 0) {
+            xAngle *= -1;
+        }
+        float yAngle = Vector3.Angle(xzRing, cameraVector);
+        if (cameraVector.y < 0) {
+            yAngle *= -1;
+        }
+
+        float magnitude = cameraVector.magnitude;
+        xAngle += angle.x;
+        yAngle += angle.y;
+        if (yAngle < 0f) {
+            yAngle += 0f - yAngle;
+        } else if (yAngle > 60f) {
+            yAngle -= yAngle - 60f ;
+        }
+        xAngle *= Mathf.Deg2Rad;
+        yAngle *= Mathf.Deg2Rad;
+        cameraVector = new Vector3(xzRing.magnitude * Mathf.Sin(xAngle), cameraVector.magnitude * Mathf.Sin(yAngle), xzRing.magnitude * Mathf.Cos(xAngle));
+        transform.position = playerTransform.position + cameraVector;
+        transform.rotation = Quaternion.LookRotation(cameraVector * -1);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = .5f;
+    public float groundCheck = 1.0f;
+    public float jumpForce = 5.0f;
+    public float addedGravity = 0.0f;
+    public GameObject camera;
+
+    private Rigidbody playerRB;
+    private bool isGrounded;
+
+    // Use this for initialization
+    void Start () {
+        playerRB = GetComponent<Rigidbody>();
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+        // Raycast underneath to see if the object is grounded
+        isGrounded = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), groundCheck);
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * groundCheck, Color.red);
+
+        // Jump check
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        // Get keyboard inputs
+        float deltaX = Input.GetAxis("Horizontal");
+        float deltaY = Input.GetAxis("Vertical");
+        Vector3 deltaMovement = new Vector3(deltaX, 0f, deltaY);
+
+        // Get the angle between the Camera Right Vector and the World Right (1, 0, 0)
+        float angleCam2Right = Vector3.Angle(Vector3.right, camera.transform.right);
+
+        // If the Z is positive flip the sign, I'm not entirely sure why this works tbh
+        if (camera.transform.right.z > 0)
+        {
+               angleCam2Right *= -1;
+        }
+        deltaMovement = Quaternion.AngleAxis(angleCam2Right, Vector3.up) * deltaMovement;
+
+
+        // Finally move the object
+        transform.Translate(deltaMovement * speed * Time.deltaTime);
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (!isGrounded)
+        {
+            playerRB.AddForce(Vector3.down * addedGravity * playerRB.mass);
+
+        }
+    }
+
+    private void Jump()
+    {   
+
+        playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
